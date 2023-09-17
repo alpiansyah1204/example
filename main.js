360e449c6b9b5e9b639eb86acc35ea9a10dc9724
alpiansyah1204 <96229888+alpiansyah1204@users.noreply.github.com>
2023-09-17 14:08:36 +0700

    new examople

---
+++ main.js
@@ -0,0 +1,39 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const bgVvideo = document.querySelector("#bgVideo");
+
+bgVideo.pause();
+bgVideo.currentTime = 0;
+
+let sections = gsap.utils.toArray(".step");
+sections.forEach((step, i) => {
+
+    ScrollTrigger.create({
+        trigger: step,
+        start: "bottom bottom",
+        end: "+=1000",
+        pin: true,
+        anticipatePin: 1,
+    });
+
+    gsap.fromTo(bgVideo, { currentTime: 3 * i }, {
+        scrollTrigger: {
+            trigger: step,
+            scrub: 2,
+            start: "top bottom",
+            end: "bottom bottom",
+        },
+        currentTime: 3 * (i + 1),
+        duration: 1,
+        ease: "none",
+
+    });
+
+});
+
+gsap.to("#bgVideo", {
+    scrollTrigger: {
+        scrub: true
+    },
+    scale: 1.5
+})
